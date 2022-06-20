9dece8d7a1d3448f69203bbd5e4265c846512acf
MatiasFontecillaUCN <matias.fontecilla@alumnos.ucn.cl>
2022-06-20 18:45:48 -0400

    Fix(Project): Fixing Ai Patrol

--- Assets/Scripts/AiPatrol.cs
+++ Assets/Scripts/AiPatrol.cs
@@ -7,17 +7,23 @@ public class AiPatrol : MonoBehaviour
 
     [SerializeField] private bool mustPatrol;
     [SerializeField] private Rigidbody2D rigidBody;
-    [SerializeField] private float enemySpeed;
+    [SerializeField] private float enemySpeed, range, timeBetweenAttack, chaseSpeed;
     [SerializeField] private Transform groundCheckPosition;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Collider2D bodyCollider;
     [SerializeField] private LayerMask wallLayer;   
+    [SerializeField] private float distanceToPlayer;
+    [SerializeField] private Transform player;
+    [SerializeField] private GameObject Proyectile;
     private bool mustFlip;
+    private bool Attacking;
 
     // Start is called before the first frame update
     void Start()
     {
         mustPatrol = true;
+        Attacking = false;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -27,12 +33,28 @@ public class AiPatrol : MonoBehaviour
         {
             Patrol();
         }
-        
+        distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if(distanceToPlayer <= range)
+        {
+            if(player.position.x > transform.position.x && transform.localScale.x < 0 || player.position.x < transform.position.x && transform.localScale.x > 0)
+            {
+                Flip();
+                chaseSpeed *= -1;
+            }
+            
+            mustPatrol = false;
+            rigidBody.velocity = Vector2.zero;
+            StartCoroutine(Attack());
+        }else
+        {
+            mustPatrol = true;
+        }
     }
 
     void FixedUpdate()
     {
-        if(mustPatrol)
+        if(mustPatrol || Attacking)
         {
             mustFlip = !Physics2D.OverlapCircle(groundCheckPosition.position, 0.1f,groundLayer);
         }
@@ -53,5 +75,16 @@ public class AiPatrol : MonoBehaviour
         transform.localScale = new Vector2(transform.localScale.x*-1,transform.localScale.y);
         enemySpeed *= -1;
         mustPatrol = true;
+    }
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(timeBetweenAttack);
+        Attacking = true;
+        rigidBody.velocity = new Vector2(chaseSpeed*Time.fixedDeltaTime, rigidBody.velocity.y);
+        if(mustFlip || bodyCollider.IsTouchingLayers(wallLayer))
+        {
+            rigidBody.velocity = Vector2.zero;
+        }
     }
 }
